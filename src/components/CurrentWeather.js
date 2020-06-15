bc7626dc05428a9741f4bf8d7abdbc5b73c1045b
mattchupp <mattlchupp@gmail.com>
2020-06-15 15:24:14 -0500

    component for displaying weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -5,25 +5,31 @@ import React from 'react';
 function CurrentWeather(props) {
   return (
     <div className="current-weather-container">
-      <div className="weather-grid">
-        <div>
-          <h1>Sol {props.sol}</h1>
-          <p>{props.date}</p>
-        </div>
-        <div>
-          <h2>Temp</h2>
-          <p>Max: {props.maxTemp} &deg;C</p>
-          <p>Min: {props.minTemp} &deg;C</p>
-        </div>
-      </div>
-      <div className="weather-grid-center">
-        <div>
-          <h2>Wind</h2>
-          <p>Wind Direction: {props.windDir}</p>
-          <p>Wind Direction Degrees: {props.windDirDegrees}</p>
-        </div>
-      </div>
+      <div className="weather-card">
+        <div className="weather-grid">
+          <div>
+            <h1>Sol {props.sol}</h1>
+            <p>{props.date}</p>
+          </div>
+          <div>
+            <h2>Temp</h2>
+            <p>Max: {props.maxTemp} &deg;C</p>
+            <p>Min: {props.minTemp} &deg;C</p>
+          </div>
       
+          <div>
+            <h2>Season</h2>
+            <p>{props.season.toUpperCase()}</p>
+          </div>
+        
+          <div>
+            <h2>Wind</h2>
+            <p>Wind Direction: {props.windDir}</p>
+            <p>Wind Direction Degrees: {props.windDirDegrees}</p>
+          </div>
+        
+        </div>
+      </div>
     </div>
   )
 }
