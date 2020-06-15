3257bf1319c4fb575962a8d0f99e7a395edb17f2
mattchupp <mattlchupp@gmail.com>
2020-06-15 11:22:20 -0500

    get weather and show current sol

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -10,7 +10,7 @@ function Forcast() {
   useEffect(() => {
     Axios.get(`https://api.nasa.gov/insight_weather/?api_key=${process.env.REACT_APP_INSIGHT_KEY}&feedtype=json&ver=1.0`)
      .then(res => {
-        // console.log(res.data); 
+        console.log(res.data); 
         setWeather(res.data)
         // console.log(weather)
         setGotData(true); 
@@ -18,16 +18,27 @@ function Forcast() {
      .then(error => {
         console.log(error); 
      })
-
+    
+     getSolData(); 
   }, [])
 
-  console.log(weather)
+  function getSolData() {
+    let weatherBySol = {
+      sol_keys: weather.sol_keys,
+      validity_checks: weather.validity_checks
+    }
+    console.log(weatherBySol)
+  }
+
 
   if(gotData) {
     return (
       <div>
         <h1>Weather</h1>
        
+        <p>Sol {weather.sol_keys[6]}</p>
+        
+      
         
   
       </div>
