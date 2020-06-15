c07ea1979a6adf29230f790425ecafa7129109c8
mattchupp <mattlchupp@gmail.com>
2020-06-15 12:06:10 -0500

    adding data to currentweather

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'; 
 import Axios from 'axios'; 
+import CurrentWeather from './CurrentWeather'; 
 
 
 function Forcast() {
 
   const [weather, setWeather] = useState([]); 
   const [gotData, setGotData] = useState(false); 
+
 
   useEffect(() => {
     Axios.get(`https://api.nasa.gov/insight_weather/?api_key=${process.env.REACT_APP_INSIGHT_KEY}&feedtype=json&ver=1.0`)
@@ -14,30 +16,32 @@ function Forcast() {
         setWeather(res.data)
         // console.log(weather)
         setGotData(true); 
+
+        // let currentSol = res.data.sol_keys[6]; 
+        // console.log(res.data[549].AT.mx)
      })
      .then(error => {
         console.log(error); 
      })
     
-    //  getSolData(); 
+  
   }, [])
-
-  // function getSolData() {
-  //   let weatherBySol = {
-  //     sol_keys: weather.sol_keys,
-  //     validity_checks: weather.validity_checks
-  //   }
-  //   console.log(weatherBySol)
-  // }
 
 
   if(gotData) {
     return (
       <div>
         <h1>Weather</h1>
-       
-        <p>Sol {weather.sol_keys[6]}</p>
+        <CurrentWeather 
+          sol={weather.sol_keys[6]}
+          date={weather[weather.sol_keys[6]].First_UTC}
+          maxTemp={weather[weather.sol_keys[6]].AT.mx}
+          minTemp={weather[weather.sol_keys[6]].AT.mn}
+          windDir={weather[weather.sol_keys[6]].WD.most_common.compass_point}
+          windDirDegrees={weather[weather.sol_keys[6]].WD.most_common.compass_degrees}
+        />
         
+        {/* <p>Max Temp {weather[weather.sol_keys[6]].AT.mx}</p> */}
       
         
   
