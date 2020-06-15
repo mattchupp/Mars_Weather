9c49b9a684f358a2e09ea63c19066ea99c1e4c85
mattchupp <mattlchupp@gmail.com>
2020-06-15 12:05:51 -0500

    adding to component for showing most recent weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,8 +4,14 @@ import React from 'react';
 
 function CurrentWeather(props) {
   return (
-    <div>
-
+    <div className="current-weather-card">
+      <h1>Sol {props.sol}</h1>
+      <p>{props.date}</p>
+      <h2>Temp</h2>
+      <p>Max: {props.maxTemp}</p>
+      <p>Min: {props.minTemp}</p>
+      <p>Wind Direction: {props.windDir}</p>
+      <p>Wind Direction Degrees: {props.windDirDegrees}</p>
     </div>
   )
 }
