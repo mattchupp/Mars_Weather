a4e061715203abe2bdc56280277b12bbd3fec274
mattchupp <mattlchupp@gmail.com>
2020-06-15 16:26:31 -0500

    deleted file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Explanation from './components/Explanation'; 
 import Forcast from './components/Forcast'; 
 
 function App() {
@@ -8,9 +7,7 @@ function App() {
 
   return (
     <div className="app-container">
-      {/* <Explanation /> */}
       <Forcast />
-      
     </div>
   );
 }
