c20a63e27d587cf5a61c34b01915306fb81b820e
mattchupp <mattlchupp@gmail.com>
2020-06-15 13:46:29 -0500

    aligning items inside weather container

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,14 +4,26 @@ import React from 'react';
 
 function CurrentWeather(props) {
   return (
-    <div className="current-weather-card">
-      <h1>Sol {props.sol}</h1>
-      <p>{props.date}</p>
-      <h2>Temp</h2>
-      <p>Max: {props.maxTemp}</p>
-      <p>Min: {props.minTemp}</p>
-      <p>Wind Direction: {props.windDir}</p>
-      <p>Wind Direction Degrees: {props.windDirDegrees}</p>
+    <div className="current-weather-container">
+      <div className="weather-grid">
+        <div>
+          <h1>Sol {props.sol}</h1>
+          <p>{props.date}</p>
+        </div>
+        <div>
+          <h2>Temp</h2>
+          <p>Max: {props.maxTemp} &deg;C</p>
+          <p>Min: {props.minTemp} &deg;C</p>
+        </div>
+      </div>
+      <div className="weather-grid-center">
+        <div>
+          <h2>Wind</h2>
+          <p>Wind Direction: {props.windDir}</p>
+          <p>Wind Direction Degrees: {props.windDirDegrees}</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
